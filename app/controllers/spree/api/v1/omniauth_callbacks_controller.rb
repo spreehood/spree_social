d2c1c03de83b1f04acda75e03c37fbe08165de54
shradhaN <shradha.neupane@yahoo.com>
2020-09-29 20:48:23 +0545

    api-for-facebook-login: return access token for omniauth_token

--- app/controllers/spree/api/v1/omniauth_callbacks_controller.rb
+++ app/controllers/spree/api/v1/omniauth_callbacks_controller.rb
@@ -6,26 +6,40 @@
     include Spree::Core::ControllerHelpers::Store
     skip_before_action :verify_authenticity_token
 
-
     def login
-      # Check for valid provider or not
+      eligible_providers = SpreeSocial::OAUTH_PROVIDERS.map { |p| p[1] if p[2] == 'true' }.compact
+
+      if !(eligible_providers.include?(auth_hash['provider']))
+        render json: {error: I18n.t('devise.omniauth_callbacks.provider_not_found', kind: auth_hash['provider'])}, status: :unprocessable_entity
+        return
+      end
+
       authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
-      if authentication.present? and authentication.try(:user).present?
-        # sign in the user and return the tokens
-        sign_in(authentication.user)
+
+      if authentication.present? && authentication.try(:user).present?
+        render json: access_token(authentication.user).body, status: :ok
       elsif spree_current_user
         spree_current_user.apply_omniauth(auth_hash)
         spree_current_user.save!
-        # Return the access token
+        render json: access_token(spree_current_user).body, status: :ok
       else
         user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
         user.apply_omniauth(auth_hash)
         if user.save
-          # sign in the user and return the access token
+          render json: access_token(user).body, status: :ok
         else
-          # Show that the user is not present in the system and send to registration page
+          render json: { error: I18n.t('spree.user_was_not_valid') }, status: :error
         end
       end
+    end
+
+    def access_token(user)
+      access_token = Doorkeeper::AccessToken.create!({
+        resource_owner_id: user.id,
+        expires_in: Doorkeeper.configuration.access_token_expires_in,
+        use_refresh_token: Doorkeeper.configuration.refresh_token_enabled?
+      })
+      Doorkeeper::OAuth::TokenResponse.new(access_token)
     end
 
     def auth_hash
