a9b3b37d43c517a72900b6c164c2259959f2bb0c
shradhaN <shradha.neupane@yahoo.com>
2020-09-30 19:39:43 +0545

    api-facebook-login: Refactor code

--- app/controllers/spree/api/v1/omniauth_callbacks_controller.rb
+++ app/controllers/spree/api/v1/omniauth_callbacks_controller.rb
@@ -1,50 +1,48 @@
+class Spree::Api::V1::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  class Spree::Api::V1::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    include Spree::Core::ControllerHelpers::Common
-    include Spree::Core::ControllerHelpers::Order
-    include Spree::Core::ControllerHelpers::Auth
-    include Spree::Core::ControllerHelpers::Store
-    skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
+  before_action :validate_provider, only: :login
 
-    def login
-      eligible_providers = SpreeSocial::OAUTH_PROVIDERS.map { |p| p[1] if p[2] == 'true' }.compact
+  def login
+    authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
 
-      if !(eligible_providers.include?(auth_hash['provider']))
-        render json: {error: I18n.t('devise.omniauth_callbacks.provider_not_found', kind: auth_hash['provider'])}, status: :unprocessable_entity
-        return
-      end
-
-      authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
-
-      if authentication.present? && authentication.try(:user).present?
-        render json: access_token(authentication.user).body, status: :ok
-      elsif spree_current_user
-        spree_current_user.apply_omniauth(auth_hash)
-        spree_current_user.save!
-        render json: access_token(spree_current_user).body, status: :ok
+    if authentication.present? && authentication.try(:user).present?
+      access_token(authentication.user)
+    elsif spree_current_user
+      spree_current_user.apply_omniauth(auth_hash)
+      spree_current_user.save!
+      access_token(spree_current_user)
+    else
+      user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
+      user.apply_omniauth(auth_hash)
+      if user.save
+        access_token(user).body
       else
-        user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
-        user.apply_omniauth(auth_hash)
-        if user.save
-          render json: access_token(user).body, status: :ok
-        else
-          render json: { error: I18n.t('spree.user_was_not_valid') }, status: :error
-        end
+        render json: { error: I18n.t('spree.user_was_not_valid') }, status: 422 and return
       end
     end
-
-    def access_token(user)
-      access_token = Doorkeeper::AccessToken.create!({
-        resource_owner_id: user.id,
-        expires_in: Doorkeeper.configuration.access_token_expires_in,
-        use_refresh_token: Doorkeeper.configuration.refresh_token_enabled?
-      })
-      Doorkeeper::OAuth::TokenResponse.new(access_token)
-    end
-
-    def auth_hash
-      params[:omniauth_callback]
-    end
+    render json: @token_response.body, status: 200
   end
 
+  def access_token(user)
+    access_token = Doorkeeper::AccessToken.create!({
+      resource_owner_id: user.id,
+      expires_in: Doorkeeper.configuration.access_token_expires_in,
+      use_refresh_token: Doorkeeper.configuration.refresh_token_enabled?
+    })
+    @token_response = Doorkeeper::OAuth::TokenResponse.new(access_token)
+  end
 
+  def auth_hash
+    params[:omniauth_callback]
+  end
+
+  def validate_provider
+    eligible_providers = SpreeSocial::OAUTH_PROVIDERS.map { |provider| provider[1] if provider[2] == 'true' }.compact
+
+    unless eligible_providers.include?(auth_hash['provider'])
+      render json: { error: I18n.t('devise.omniauth_callbacks.provider_not_found', kind: auth_hash['provider']) },
+             status: 422
+    end
+  end
+end
